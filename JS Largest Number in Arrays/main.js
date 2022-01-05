dca9293d7fd33645879e39604b1c4ca2da782b51
Van Tran <tavan.it@gmail.com>
2022-01-05 12:52:24 +0100

    update new method

--- JS Largest Number in Arrays/main.js
+++ JS Largest Number in Arrays/main.js
@@ -1,3 +1,34 @@
+// -------------------- method 4 ----------------------------- //
+let array = [[40, 5, 1, 3], [20, 10, 22, 19, 50], [113, 200, 118, 26], [32, 335, 37, 39], [100, 1001, 8555, 1]];
+function findMax(arr){
+    //creat empty array for show result
+    let result = []
+    //a for loop for going to inside of main array 
+    for (let i = 0; i < arr.length; i++) {
+        //find max number in each index of main array 
+        let max = Math.max.apply(null,arr[i])
+        //add max number in each index to result array  
+        result.push(max)
+    }
+    console.log(result);
+}
+findMax(array)
+// ----------------------------------------------------------- //
+// -------------------- method 3 ----------------------------- //
+let array = [[40, 5, 1, 3], [20, 10, 22, 19, 50], [113, 200, 118, 26], [32, 335, 37, 39], [100, 1001, 8555, 1]];
+function findMax(arr) {
+    //creat empty array for show result
+    let result = []
+    //a for loop for going to inside of main array 
+    for (let i = 0; i < arr.length; i++) {
+        // we are going to sort each index of array 
+        let sort = arr[i].sort(function(a, b){return a - b}) ;
+        //add max number in result array 
+        result.push(sort[sort.length-1])
+    }
+    console.log(result)
+}
+// ------------------------------------------------------------//
 // -------------------- method 2 ----------------------------- //
 let array = [[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]];
 console.log(array[0]);
