e7b3bae1cef6e3180183bd58aae65498157f4729
Van Tran <tavan.it@gmail.com>
2022-01-07 12:53:38 +0100

    update Title Case a Sentence

--- JS Title Case a Sentence/main.js
+++ JS Title Case a Sentence/main.js
@@ -1,19 +1,31 @@
 // Title Case a Sentence
 // Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
 function titleCase(str){
-    let arrayX = str.split(' ');
+    let strLowerCase = str.toLowerCase();
+    let arrayX = strLowerCase.split(' ');
     let newStr = '';
     for (let index = 0; index<arrayX.length; index++){
         newWord = arrayX[index];
-        newWord = newWord.toLowerCase();
         newWord = newWord.charAt(0).toUpperCase() + newWord.slice(1);
-        // console.log(newWord);
         newStr += newWord + ' ';
     }
-    console.log(newStr);
+    return newStr;
 };
-titleCase("I'm a little tea pot");
-titleCase('sHoRt AnD sToUt')
-// titleCase("i'm a little tea pot");
-// titleCase("sHoRt AnD sToUt");
+console.log(titleCase("I'm a little tea pot")); 
+console.log(titleCase('sHoRt AnD sToUt'));
 
+
+// let titleCase="I'm a little tea pot"
+// function capitalize(cap){
+//   let lower = cap.toLowerCase();
+//   let newTitleCase = lower.split (" ")
+
+//   for(let i =0 ; i<newTitleCase.length; i++){
+//   const word= newTitleCase[i]
+//   const newWord= word.charAt(0).toUpperCase()+ word.slice(1)
+//   newTitleCase[i]=newWord
+//   }
+//   return newTitleCase.join(" "); // convert array to string
+// }
+
+// console.log(capitalize(titleCase))
