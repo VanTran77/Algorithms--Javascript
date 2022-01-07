cfffa61ec2fb1542491d4234f8f8a1c11500cc14
Van Tran <tavan.it@gmail.com>
2022-01-07 10:47:02 +0100

    updated Sum All Numbers in a Range

--- JS Sum All Numbers in a Range/main.js
+++ JS Sum All Numbers in a Range/main.js
@@ -5,23 +5,42 @@
 let arr1 = [1,4];
 let arr2 = [4,1];
 let arr3 = [5,10];
+let arr4 = [10,5];
 
-let sumAll = arr => {
-    return sumArr =  arr.reduce((total, amount) => { 
-        let min = total;
-        let max = amount;
-        if(min > amount){ 
-          min = amount; max=total;
-        };
-        total += amount;
-        for(let i=min+1; i<max; i++){
-          total += i;
-        }
-        return total;
-    });
-}
+function sumAll(arr)  {
+    let min = arr[0];
+    let max = arr[1];
+    if (min > arr[1]){
+      min  = arr[1];
+      max =  arr[0];
+      // console.log(min, max);
+    } 
+    let sum = min;
+    for (let i=min+1; i<=max; i++){
+      sum += i;
+    } 
+    // console.log(sum);
+    return sum;
+  }
 
 console.log(sumAll(arr1));
 console.log(sumAll(arr2));
 console.log(sumAll(arr3));
+console.log(sumAll(arr4));
 
+
+// --------------- second method -------------------- //
+// let sumAll = arr => {
+//   return sumArr =  arr.reduce((total, amount) => { 
+//       let min = total;
+//       let max = amount;
+//       if(min > amount){ 
+//         min = amount; max=total;
+//       };
+//       total += amount;
+//       for(let i=min+1; i<max; i++){
+//         total += i;
+//       }
+//       return total;
+//   });
+// }
--- JS Title Case a Sentence/main.js
+++ JS Title Case a Sentence/main.js
@@ -1,18 +1,19 @@
 // Title Case a Sentence
 // Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
 function titleCase(str){
-    let words = str.split(' ');
-    let newStr = "";
-    for (let i = 0; i<words.length; i++){
-        newWord = words[i];
+    let arrayX = str.split(' ');
+    let newStr = '';
+    for (let index = 0; index<arrayX.length; index++){
+        newWord = arrayX[index];
         newWord = newWord.toLowerCase();
         newWord = newWord.charAt(0).toUpperCase() + newWord.slice(1);
+        // console.log(newWord);
         newStr += newWord + ' ';
     }
     console.log(newStr);
 };
 titleCase("I'm a little tea pot");
-titleCase("sHoRt AnD sToUt")
+titleCase('sHoRt AnD sToUt')
 // titleCase("i'm a little tea pot");
 // titleCase("sHoRt AnD sToUt");
 
