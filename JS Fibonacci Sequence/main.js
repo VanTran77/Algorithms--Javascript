3dc86ae190fe5f4f19ebe1cd79be5d71fc9a73fe
Van Tran <tavan.it@gmail.com>
2022-01-07 11:04:50 +0100

    updated Fibonacci Sequence

--- JS Fibonacci Sequence/main.js
+++ JS Fibonacci Sequence/main.js
@@ -1,7 +1,11 @@
+// Fibonacci Sequence
+// Immediately you find the pattern: setting the first two terms to be 1, any other term is the sum of the two previous terms. 1+1=2, 1+2=3, 2+3=5, 3+5=8, 5+8=13 and so on.
+// In this way, if n would be equal to 4 your result should be 3; but if n is 7, then the result is 13.
+
 let n = 4;
 let n1 = 7;
 let n2 = 43;
-function nthFibonacci(n){
+function FibonacciSeq(n){
     if(n <= 2){
       return 1;
     } else{
@@ -17,6 +21,6 @@ function nthFibonacci(n){
      
     }
   }
-  console.log(nthFibonacci(n));
-  console.log(nthFibonacci(n1));
-  console.log(nthFibonacci(n2));
+  console.log(FibonacciSeq(n));
+  console.log(FibonacciSeq(n1));
+  console.log(FibonacciSeq(n2));
